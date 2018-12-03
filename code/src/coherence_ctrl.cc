81e9232d0eb5c181966fb429332ef5fdd82c26dd
Soumil Heble <soumil.heble@gmail.com>
2018-12-03 02:05:37 -0500

    MESI Cache Coherence Protocol, Cache2Cache Count Bug

--- code/src/coherence_ctrl.cc
+++ code/src/coherence_ctrl.cc
@@ -127,9 +127,7 @@ void coherenceController::processMSI(ulong procNum, uchar rdWr, ulong reqAddr)
         /** Set To Hit */
         hitMiss = HIT;
         
-        /** Record Initial Flag of the Cache Line */
-//         initialFlag = line->getFlags();
-        
+        /** If Hit Cache Line in Shared State and a Write is Required */
         if((line->getFlags()==SHARED)&&(rdWr==WR_REQ))
         {
             /* Bus in Control of Current Processor */
@@ -174,9 +172,10 @@ void coherenceController::processMSI(ulong procNum, uchar rdWr, ulong reqAddr)
                                 /** Update Flush Counter */
                                 cacheOnbus[loop_i]->incFlush();
                                 
+                                /** Flush Constitutes as a Memory Transaction */
                                 cacheOnbus[loop_i]->incMemtransactions();
                                 
-                                /** Update Writeback Counter */
+                                /** Flush Constitutes as a Writeback */
                                 cacheOnbus[loop_i]->incWB();
                                 
                                 busControl = loop_i;
@@ -203,9 +202,10 @@ void coherenceController::processMSI(ulong procNum, uchar rdWr, ulong reqAddr)
                                         /** Update Flush Counter */
                                         cacheOnbus[loop_i]->incFlush();
                                         
+                                        /** Flush Constitutes as a Memory Transaction */
                                         cacheOnbus[loop_i]->incMemtransactions();
                                         
-                                        /** Update Writeback Counter */
+                                        /** Flush Constitutes as a Writeback */
                                         cacheOnbus[loop_i]->incWB();
                                         
                                         busControl = loop_i;
@@ -226,21 +226,12 @@ void coherenceController::processMSI(ulong procNum, uchar rdWr, ulong reqAddr)
     /** Perform Finishing Actions */
     if(hitMiss==HIT)
     {
-        //ulong tag;
-        //tag = cacheOnbus[procNum]->calcTag(reqAddr);
-        //line->setTag(tag);
-        
         cacheOnbus[procNum]->updateLRU(line);
         
         if(rdWr==WR_REQ)
         {
             line->setFlags(MODIFIED);
-//             finalFlag = MODIFIED;
         }
-//         else
-//         {
-//             finalFlag = SHARED;
-//         }
     }
     else
     {
@@ -253,12 +244,10 @@ void coherenceController::processMSI(ulong procNum, uchar rdWr, ulong reqAddr)
         if(rdWr==WR_REQ)
         {
             line->setFlags(MODIFIED);
-//             finalFlag = MODIFIED;
         }
         else
         {
             line->setFlags(SHARED);
-//             finalFlag = SHARED;
         }
     }
     
@@ -269,22 +258,294 @@ void coherenceController::processMSI(ulong procNum, uchar rdWr, ulong reqAddr)
     busCommand = INVALID_BUS;
     
     hitMiss = RST_OUT;
-    
-    /** Update Invalidation or Intervention Counter */
-//     if(finalFlag==INVALID)  /** It's Invalidation */
-//     {
-//         cacheOnbus[procNum]->incInval();
-//     }
-//     else
-//     if((initialFlag==MODIFIED)&&(finalFlag==SHARED))    /** It's Intervention */
-//     {
-//         cacheOnbus[procNum]->incInterv();
-//     }
 }
 
 void coherenceController::processMESI(ulong procNum, uchar rdWr, ulong reqAddr)
 {
-    printf("ERROR: MESI\n");
+    /** Increment the Cache's Request Count */
+    cacheOnbus[procNum]->inccurrentCycle();
+    
+    /** Pointer to Cache Line to Update Finally */
+    cacheLine *line;
+    
+    /** Check Type of Read and Update Counter */
+    if(rdWr==RD_REQ)
+    {
+        cacheOnbus[procNum]->incReads();
+    }
+    else
+    {
+        cacheOnbus[procNum]->incWrites();
+    }
+    
+    /** Look for the Requested Address in the Cache */
+    line = cacheOnbus[procNum]->findLine(reqAddr);
+    
+    /** If Cache Line Not Found */
+    if(line==NULL)
+    {
+        /** Set To Miss */
+        hitMiss = MISS;
+        
+        /** Fetch a Victim Cache Line for the Data */
+        cacheLine *victim = cacheOnbus[procNum]->findLineToReplace(reqAddr);
+        assert(victim != 0);
+        
+        /** Store Victim Block to the Cache Line to Update Pointer */
+        line = victim;
+        
+        if(victim->getFlags()==MODIFIED)
+        {
+            /** Update Writeback Counter */
+            cacheOnbus[procNum]->incWB();
+            
+            /** Update Memory Transaction Counter */
+            cacheOnbus[procNum]->incMemtransactions();
+        }
+        
+        /* Bus in Control of Current Processor */
+        busControl = procNum;
+        
+        /* Bus Signals are Valid */
+        busValid = VALID_BUS;
+        
+        /* Place Required Address on the Bus */
+        busAddr = reqAddr;
+        
+        if(rdWr==RD_REQ)
+        {
+            /** Update Read Miss Counters */
+            cacheOnbus[procNum]->incRM();
+            
+            /** Place BusRd Command on Bus */
+            busCommand = BUSRD;
+            
+            /** Update BusRd Counter */
+            cacheOnbus[procNum]->incBusrd();
+        }
+        else
+        {
+            /** Update Write Miss Counters */
+            cacheOnbus[procNum]->incWM();
+            
+            /** Place BusRdX Command on Bus */
+            busCommand = BUSRDX;
+            
+            /** Update BusRdX Counter */
+            cacheOnbus[procNum]->incBusrdx();
+        }
+    }
+    else
+    {
+        /** Set To Hit */
+        hitMiss = HIT;
+        
+        /** If Hit Cache Line in Shared State and a Write is Required */
+        if((line->getFlags()==SHARED)&&(rdWr==WR_REQ))
+        {
+            /* Bus in Control of Current Processor */
+            busControl = procNum;
+            
+            /* Bus Signals are Valid */
+            busValid = VALID_BUS;
+            
+            /* Place Required Address on the Bus */
+            busAddr = reqAddr;
+            
+            /** Place BusRdX Command on Bus */
+            busCommand = BUSUPGR;
+            
+            /** Update BusRdX Counter */
+            cacheOnbus[procNum]->incBusupdupgr();
+        }
+    }
+    
+    /* Perform Bus Snooping Operations */
+    if(busValid==VALID_BUS)
+    {
+        uchar loop_i;
+        
+        switch(busCommand)
+        {
+            case BUSRD: for(loop_i=0; loop_i<num_processors; loop_i++)
+                        {
+                            /** Look for the Request Address in the Cache */
+                            cacheLine *line_procn = cacheOnbus[loop_i]->findLine(busAddr);
+                            
+                            /** If Found in Cache Other than Cache Serivicing the Request */
+                            if((line_procn!=NULL)&&(loop_i!=procNum))
+                            {
+                                /** Copies Exist */
+                                copiesExist = CEX;
+                                
+                                switch(line_procn->getFlags())
+                                {
+                                    case MODIFIED:  /** Update Flush Counter */
+                                                    cacheOnbus[loop_i]->incFlush();
+                                                    
+                                                    cacheOnbus[procNum]->incMemtransactions();
+                                                    
+                                                    /** Flush Constitutes as a Memory Transaction */
+                                                    cacheOnbus[loop_i]->incMemtransactions();
+                                                    
+                                                    /** Flush Constitutes as a Writeback */
+                                                    cacheOnbus[loop_i]->incWB();
+                                                    
+                                                    /** Update Intervention Counter */
+                                                    cacheOnbus[loop_i]->incInterv();
+                                                    
+                                                    busControl = loop_i;
+                                                    busCommand = FLUSH;
+                                                    
+                                                    break;
+                                                    
+                                    case EXCLUSIVE: /** Update Flush Counter */
+                                                    cacheOnbus[loop_i]->incCache2cache();
+                                                
+                                                    /** Update Intervention Counter */
+                                                    cacheOnbus[loop_i]->incInterv();
+                                                
+                                                    busControl = loop_i;
+                                                    busCommand = FLUSHOPT;
+                                                
+                                                    break;
+                                                    
+                                    default:    /** Update Flush Counter */
+                                                cacheOnbus[loop_i]->incCache2cache();
+                                            
+                                                busControl = loop_i;
+                                                busCommand = FLUSHOPT;
+                                            
+                                                break;
+                                }
+                                
+                                /** Set Cache Line State To Shared */
+                                line_procn->setFlags(SHARED);
+                            }
+                        }
+                        
+                        if(busControl==procNum)
+                        {
+                            cacheOnbus[procNum]->incMemtransactions();
+                            copiesExist = NCEX;
+                        }
+                        break;
+                        
+            case BUSRDX:    for(loop_i=0; loop_i<num_processors; loop_i++)
+                            {
+                                /** Look for the Request Address in the Cache */
+                                cacheLine *line_proc = cacheOnbus[loop_i]->findLine(busAddr);
+                                
+                                /** If Found in Cache Other than Cache Serivicing the Request */
+                                if((line_proc!=NULL)&&(loop_i!=procNum))
+                                {
+                                    /** Update Invalidation Counter */
+                                    cacheOnbus[loop_i]->incInval();
+                                    
+                                    busControl = loop_i;
+                                    busCommand = FLUSHOPT;
+                                    
+                                    /** If Modified */
+                                    if(line_proc->getFlags()==MODIFIED)
+                                    {
+                                        /** Update Flush Counter */
+                                        cacheOnbus[loop_i]->incFlush();
+                                        
+                                        /** Flush Constitutes as a Memory Transaction */
+                                        cacheOnbus[loop_i]->incMemtransactions();
+                                        
+                                        cacheOnbus[procNum]->incMemtransactions();
+                                        
+                                        /** Flush Constitutes as a Writeback */
+                                        cacheOnbus[loop_i]->incWB();
+                                        
+                                        busControl = loop_i;
+                                        busCommand = FLUSH;
+                                    }
+                                    else
+                                    {
+                                        /** Update Flush Counter */
+                                        cacheOnbus[loop_i]->incCache2cache();
+                                    }
+                                    
+                                    /** Invalidate Cache Line */
+                                    line_proc->invalidate();
+                                }
+                            }
+                            
+                            if(busControl==procNum)
+                            {
+                                cacheOnbus[procNum]->incMemtransactions();
+                            }
+                            break;
+                            
+            case BUSUPGR:   for(loop_i=0; loop_i<num_processors; loop_i++)
+                            {
+                                /** Look for the Request Address in the Cache */
+                                cacheLine *line_procn = cacheOnbus[loop_i]->findLine(busAddr);
+                                
+                                /** If Found in Cache Other than Cache Serivicing the Request */
+                                if((line_procn!=NULL)&&(loop_i!=procNum)&&(line_procn->getFlags()==SHARED))
+                                {
+                                    /** Invalidate Cache Line */
+                                    line_procn->invalidate();
+                                    
+                                    /** Update Invalidation Counter */
+                                    cacheOnbus[loop_i]->incInval();
+                                }
+                            }
+                            
+                            break;
+                                    
+            
+            default:    printf("ERROR\n");
+                        break;
+        }
+    }
+    
+    /** Perform Finishing Actions */
+    if(hitMiss==HIT)
+    {
+        cacheOnbus[procNum]->updateLRU(line);
+        
+        if(rdWr==WR_REQ)
+        {
+            line->setFlags(MODIFIED);
+        }
+    }
+    else
+    {
+        ulong tag;
+        tag = cacheOnbus[procNum]->calcTag(reqAddr);
+        line->setTag(tag);
+        
+        cacheOnbus[procNum]->updateLRU(line);
+        
+        if(rdWr==WR_REQ)
+        {
+            line->setFlags(MODIFIED);
+        }
+        else
+        {
+            if(copiesExist==NCEX)
+            {
+                line->setFlags(EXCLUSIVE);
+            }
+            else
+            {
+                line->setFlags(SHARED);
+            }
+        }
+    }
+    
+    /** Reset Bus */
+    busControl = 0xFF;
+    busValid = INVALID_BUS;
+    busAddr = 0xFFFFFFFF;
+    busCommand = INVALID_BUS;
+    copiesExist = NCEX;
+    
+    hitMiss = RST_OUT;
 }
 
 void coherenceController::processDRAGON(ulong procNum, uchar rdWr, ulong reqAddr)
@@ -311,21 +572,5 @@ void coherenceController::dumpMetrics()
         printf("10. number of invalidations:%24lu\n", cacheOnbus[loop_i]->getInval());
         printf("11. number of flushes:%28lu\n", cacheOnbus[loop_i]->getFlush());
         printf("12. number of BusRdX:%30lu\n", cacheOnbus[loop_i]->getBusrdx());
-        
-        /*
-        printf("============ Simulation results (Cache %u) ============\n",(uint)loop_i);
-        printf("01. number of reads:%lu\n", cacheOnbus[loop_i]->getReads());
-        printf("02. number of read misses:%lu\n", cacheOnbus[loop_i]->getRM());
-        printf("03. number of writes:%lu\n", cacheOnbus[loop_i]->getWrites());
-        printf("04. number of write misses:%lu\n", cacheOnbus[loop_i]->getWM());
-        printf("05. total miss rate:%f\n", ((float)(cacheOnbus[loop_i]->getRM()+cacheOnbus[loop_i]->getWM()))*100.0/((float)(cacheOnbus[loop_i]->getReads()+cacheOnbus[loop_i]->getWrites())));
-        printf("06. number of writebacks:%lu\n", cacheOnbus[loop_i]->getWB());
-        printf("07. number of cache-to-cache transfers:%lu\n", cacheOnbus[loop_i]->getCache2cache());
-        printf("08. number of memory transactions:%lu\n", cacheOnbus[loop_i]->getMemtransactions());
-        printf("09. number of interventions:%lu\n", cacheOnbus[loop_i]->getInterv());
-        printf("10. number of invalidations:%lu\n", cacheOnbus[loop_i]->getInval());
-        printf("11. number of flushes:%lu\n", cacheOnbus[loop_i]->getFlush());
-        printf("12. number of BusRdX:%lu\n", cacheOnbus[loop_i]->getBusrdx());
-        */
     }
 }
